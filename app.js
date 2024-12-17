ac4815ddb0b08fdd8acba35ab8c265a0277f12fa
Victor Eduardo <VictorOdarp@gmail.com>
2024-12-16 21:40:10 -0300

    animações sendo modificadas para que apareçam durante o scroll

--- app.js
+++ app.js
@@ -5,3 +5,18 @@ perguntas.forEach(pergunta => {
         pergunta.classList.toggle("active");
     });
 })
+
+const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+        else {
+            entry.target.classList.remove("show");
+        }
+    });
+});
+
+const elements = document.querySelectorAll(".hidden");
+elements.forEach((element) => myObserver.observe(element));
+
